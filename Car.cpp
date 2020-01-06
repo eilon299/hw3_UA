59b7dc10454ec78b9d26501c0737a2f5fb070885
Shachar Katz <shachar373@gmail.com>
2020-01-06 13:51:51 +0200

    car added

---
+++ Car.cpp
@@ -0,0 +1,28 @@
+//
+// Created by shach on 04/01/2020.
+//
+
+#include "Car.h"
+#include <string.h>
+using std::string;
+
+Car::Car(const Car& car):
+        name(car.name),
+        year(car.year) {
+}
+
+Car::Car(const string& name, int year) :
+        name(name),
+        year(year) {
+}
+
+
+ostream& operator<<(ostream& os, const Car& car) {
+    return os << car.name << " ==>> " << car.year;
+}
+
+bool compareCars (const Car& first, const Car& second) {
+    return ((first.name== second.name) && (first.year == second.year));
+
+}
+
---
+++ Car.h
@@ -0,0 +1,27 @@
+//
+// Created by shach on 04/01/2020.
+//
+#include <iostream>
+#include <string>
+using std::string;
+using std::cout;
+using std::cin;
+using std::ostream;
+
+#ifndef CAR_H
+#define CAR_H
+
+class Car {
+    string name;
+    int year;
+
+public:
+    Car(const Car& car);;
+    Car(const string& name, int year);
+    friend ostream& operator<<(ostream&, const Car& car);
+    friend bool compareCars (const Car& first, const Car& second);
+};
+
+bool compareCars (const Car& first, const Car& second);
+
+#endif //UNTITLED_CAR_H
